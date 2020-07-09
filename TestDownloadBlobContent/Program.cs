411844e3e74d2634412edea1de4acc2321d3beae
Chhavi Goel <chgoel@microsoft.com>
2020-07-09 17:57:12 +0530

    first commit

---
+++ TestDownloadBlobContent/Program.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using Microsoft.WindowsAzure.Storage.RetryPolicies;
+using System;
+using System.IO;
+
+namespace TestDownloadBlobContent
+{
+    class Program
+    {
+        public static async System.Threading.Tasks.Task Main()
+        {
+            try
+            {
+                string storageAccountConnectionString = "TO DO- CONNECTION STRING";
+                string containerName = "test-downloadcontent";
+                string blobName = "UsingXMLDoc1.xml"; //"UsingFileStream1.xml";
+
+                //string contentUsingDownloadTextAsync = await DownloadContentAsync_DownloadTextAsync(storageAccountConnectionString, containerName, blobName);
+                string contentUsingDownloadTOStreamAsync = await DownloadContentAsync_DownloadToStreamAsync(storageAccountConnectionString, containerName, blobName);
+            }
+            catch(Exception e)
+            {
+
+            }
+        }
+
+        private static CloudBlobContainer GetContainer(string storageAccountConnectionString, string containerName)
+        {
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(storageAccountConnectionString);
+
+            // Create the blob client.
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+            CloudBlobContainer container = blobClient.GetContainerReference(containerName);
+            return container;
+        }
+
+        private static async System.Threading.Tasks.Task<string> DownloadContentAsync_DownloadTextAsync(string storageAccountConnectionString, string containerName, string blobName)
+        {
+            CloudBlobContainer container = GetContainer(storageAccountConnectionString, containerName);
+            ICloudBlob blob = await container.GetBlobReferenceFromServerAsync(blobName);
+
+            // Download the blob content
+            string xmlBlobContent =
+                await (blob as CloudBlockBlob).DownloadTextAsync(
+                    null,
+                    new BlobRequestOptions { LocationMode = LocationMode.PrimaryThenSecondary },
+                    new OperationContext());
+
+            return xmlBlobContent;
+        }
+
+        private static async System.Threading.Tasks.Task<string> DownloadContentAsync_DownloadToStreamAsync(string storageAccountConnectionString, string containerName, string blobName)
+        {
+            CloudBlobContainer container = GetContainer(storageAccountConnectionString, containerName);
+            ICloudBlob blob = await container.GetBlobReferenceFromServerAsync(blobName);
+
+            // Download the blob content
+            MemoryStream resultStream = new MemoryStream();
+            await (blob as CloudBlockBlob).DownloadToStreamAsync(
+                resultStream,
+                null,
+                new BlobRequestOptions { LocationMode = LocationMode.PrimaryThenSecondary },
+                new OperationContext());
+            string xmlBlobContent = System.Text.Encoding.UTF8.GetString(resultStream.ToArray());
+
+            return xmlBlobContent;
+        }
+    }
+}
---
+++ XMLGenerator/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace XMLGenerator
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int noOfFiles = 1;
+
+            try
+            {
+
+                List<List<ContractClass>> objectsToSerialize = new List<List<ContractClass>>();
+                for (int i = 1; i <= noOfFiles; ++i)
+                {
+                    List<ContractClass> contractData = new List<ContractClass>();
+                    var contract1 = new ContractClass();
+                    contract1.Prop1 = $"file{i}";
+                    contract1.Prop2 = $"file{i}";
+
+                    contractData.Add(contract1);
+                    objectsToSerialize.Add(contractData);
+                }
+
+
+                int count = 1;
+                foreach (List<ContractClass> contractData in objectsToSerialize)
+                {
+                    var stringwriter = new System.IO.StringWriter();
+                    var serializer = new XmlSerializer(typeof(List<ContractClass>), new XmlRootAttribute("ContractClasses"));
+
+                    //1. Using FileStream
+                    //string fileName = String.Format(@"C:\TestBlobDownloadContent\UsingFileStream" + count + ".xml");
+                    //var file = new FileStream(fileName, FileMode.OpenOrCreate);
+                    //serializer.Serialize(file, contractData);
+                    //file.Close();
+
+                    //2. Using XMLDoc
+                    serializer.Serialize(stringwriter, contractData);
+                    var xmlString = stringwriter.ToString();
+                    XmlDocument doc = new XmlDocument();
+                    doc.LoadXml(xmlString);
+                    doc.PreserveWhitespace = true;
+                    string fileName = String.Format(@"C:\TestBlobDownloadContent\UsingXMLDoc" + count + ".xml");
+                    doc.Save(fileName);
+                    count++;
+                }
+
+                Console.WriteLine("Done generating XMLs..");
+                Console.ReadLine();
+            }
+            catch (Exception e)
+            {
+                throw;
+            }
+        }
+    }
+}
